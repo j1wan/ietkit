2046e56ee477f87ec65f9dd646d59d6bfa5e778b
j1wan <jiwanjeong@gmail.com>
2016-10-13 17:19:37 +0900

    improve visulation

--- R/params.R
+++ R/params.R
@@ -4,7 +4,6 @@
 #.param.int <- list(from = 0, to = 28, n = 512, x = seq(0, 28, length.out = 512), dx = 28/511)
 
 #' @export
-#'
 .param.int <- list(from = -4, to = 28, n = 512, x = seq(-4, 28, length.out = 512), dx = 32/511)
 
 #.param.tod <- list(from = circular(5/2*pi), to = circular(pi/2), n = 256, x = seq(0, 24, length.out = 256), dx = 24/255, bw = 24 * 2)
@@ -14,7 +13,6 @@
 
 ### DENSITY X ###
 #' @export
-#'
 coor.int <- function(y) return(cbind(.param.int$x, y))
 # coor.tod <- function(y) return(cbind(.param.tod$x, y))
 # coor.tow <- function(y) return(cbind(.param.tow$x, y))
--- R/plot.R
+++ R/plot.R
@@ -4,23 +4,28 @@
 #' @param intervals a vector of inter-event times
 #' @export
 #'
-HistInterval <- function(intervals, main=''){ # TODO: add log
+HistInterval <- function(intervals, main='', ...){ # TODO: add log
   intervals[intervals==0] = 1
   hist(
     log(intervals, 2),
-    xlim=c(-4, 28), ylim=c(0, 0.3), breaks=0:30, probability=T,
+    xlim = log2(c(1, 3600*24*365)),
+    ylim=c(0, 0.3),
+    breaks=0:30, probability=T,
     main=main,
     axes=F,
     border=NA,
-    col='gray', xaxs='i' #, yaxs='i'
+    col='gray', xaxs='i', #, yaxs='i'
+    ...
   )
-  axis(2)
-  axis(
-    1,
-    log(c(.1, 1, 10, 60, 600, 3600, 3600*24, 3600*24*7, 3600*24*7*10, 3600*24*7*52),2),
-    c('0.1s', '1s', '10s', '1m', '10m', '1h', '1d', '1w', '10w', '1y'))
+  axis(2, ...)
+  axis(1, .kTimeAt, .kTimeLab, ...)
 }
 
+#' @export
+.kTimeAt <- log2(c(1, 60, 3600, 3600*24, 3600*24*7, 3600*24*30, 3600*24*365))
+
+#' @export
+.kTimeLab <- c('1sec', '1min', '1hr', '1d', '1w', '1m', '1yr')
 
 
 
@@ -44,27 +49,20 @@ HistInterval <- function(intervals, main=''){ # TODO: add log
 #' Plot timestamps with vertical lines
 #' @param UNIX timestamps/epochs of an event series
 #' @export
-PlotEvent <- function(timestamps, ...) {
-  timestamps <- as.POSIXct(timestamps, origin = '1970-01-01')
+PlotEvent <- function(timestamps, alpha = 1/4, ...) {
+  #timestamps <- as.POSIXct(timestamps, origin = '1970-01-01')
   plot(NULL,
        xlim = range(timestamps),
        ylim = c(0, 1),
        axes = F,
-       ylab = '', ...)
-  abline(v = timestamps, col = rgb(0, 0, 0, 1/4))
+       ylab = '',
+       xaxs = 'i',
+       yaxs = 'i',
+       ...)
 
-  axis(1,
-       at = axTicks(1),
-       labels = as.Date(as.POSIXct(axTicks(1), origin = '1970-01-01')))
-  box()
+  abline(v = timestamps, col = rgb(20/255, 135/255, 200/255, alpha = alpha), ...)
 
-  if (FALSE) {
-    text(axTicks(1), par('usr')[3] - 0.15, pos = 3, xpd = TRUE, offset = 0,
-         labels = as.Date(as.POSIXct(axTicks(1), origin = '1970-01-01')),
-         srt = 30)
-  }
 
+  box(...)
 }
-?text
-?axis
 # PlotEvent(1000000000 + sample.int(500000000, 500), main = 'AA')
